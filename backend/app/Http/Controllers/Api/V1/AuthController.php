3d7bbed473b73f4ace48c4e043a95767d84729f3
Eternatus01 <mr.drdioyt@gmail.com>
2025-11-20 14:21:08 +0500

    Implemented on the backend: user authentication with registration, login and logout functions;

--- backend/app/Http/Controllers/Api/V1/AuthController.php
+++ backend/app/Http/Controllers/Api/V1/AuthController.php
@@ -3,23 +3,90 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\LoginUserRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function register(StoreUserRequest $request){
-        return 'Register';
+        try {
+            $data = $request->only(['name', 'email', 'password']);
+
+            $user = User::create($data);
+
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'user'=> [
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'email' => $user->email,
+                ],
+                'token' => $token,
+            ], 201);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Ошибка регистрации. Попробуйте позже.',
+                'debug' => $e->getMessage(), // только для dev
+            ], 500);
+        }
     }
 
     public function login(LoginUserRequest $request){
-        return 'Login';
+        try {
+            $credentials = $request->only(['email', 'password']);
 
+            if(!Auth::attempt($credentials)){
+                return response()->json([
+                    'message' => 'Неверное имя пользователя или пароль'
+                ], 401);
+            }
+
+            $user = User::where('email', $request->email)->first();
+            $user->tokens()->delete();
+
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'user' => [
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'email' => $user->email,
+                ],
+                'token' => $token,
+            ]);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Ошибка при попытке входа. Попробуйте позднее.',
+                'debug' => $e->getMessage(), // только для dev
+            ], 500);
+        }
     }
 
-    public function logout(Request $request){
-        return 'Logout';
+    public function logout(){
+        try {
+            $user = Auth::user();
 
+            if (!$user) {
+                return response()->json([
+                    'message' => 'Пользователь не авторизован'
+                ], 401);
+            }
+
+            Auth::user()->currentAccessToken()->delete();
+    
+            return response()->json([
+                'message' => 'Вы успешно вышли'
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Ошибка выхода. Попробуйте позже.',
+                'debug' => $e->getMessage(), // только для dev
+            ], 500);
+        }
+        
     }
 }
--- backend/app/Http/Requests/StoreUserRequest.php
+++ backend/app/Http/Requests/StoreUserRequest.php
@@ -24,7 +24,12 @@ class StoreUserRequest extends FormRequest
         return [
             'name' => 'required|string|max:255',
             'email' => 'required|email|max:255|unique:users,email',
-            'password' => 'required|string|min:6',
+            'password' => [
+                'required',
+                'string',
+                'min:8',
+                // 'regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&]).+$/'
+            ],
         ];
     }
 }
